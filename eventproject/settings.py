1eed663c1cf8ddfb75bf5abda8c8fe78a8575517
fernao <fernaomouzinho30@gmail.com>
2023-02-21 05:49:25 +0900

    Update

--- eventproject/settings.py
+++ eventproject/settings.py
@@ -91,22 +91,22 @@ WSGI_APPLICATION = 'eventproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'adngovtl_agendafinal',
-        'USER': 'eventdb',
-        'PASSWORD': 'adn*&###3',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'adngovtl_agendafinal',
+    #     'USER': 'eventdb',
+    #     'PASSWORD': 'adn*&###3',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    #     },
+    # }
 }
 
 
